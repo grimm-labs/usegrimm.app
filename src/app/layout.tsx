2f389c7a592ca2a6d4b2c4fbd571d766c787096d
nejos97 <nenbajonathan@protonmail.com>
2024-12-09 03:58:43 +0300

    chore: setup landing page

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Grimm - The only Bitcoin Wallet you need",
+  description: "Experience the future of finance with Grimm. Send, receive, and spend bitcoin effortlessly wherever you are. Enjoy seamless transactions with absolutely zero fees!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-white flex items-center justify-items-center min-h-screen w-100">
+      <div className="text-3xl text-black border">
+        Welcome to Grimm Wallet!
+      </div>
+    </div>
+  );
+}
