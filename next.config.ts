b9b593c9d85ecaa89cb0923372e19296e93b3da1
nejos97 <nenbajonathan@protonmail.com>
2025-01-16 15:55:28 +0100

    fix GP issue by providing basePaht and assetsPrefix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/usegrimm.app",
+  assetPrefix: "/usegrimm.app",
 };
 
 export default nextConfig;
