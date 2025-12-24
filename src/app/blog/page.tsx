7abfcb24442d19a8bdd635a05fe55bcd40ef5dae
Baudouin Dev <120689453+boris913@users.noreply.github.com>
2025-12-24 13:28:18 +0100

    fix():  error: 'BlogPostMetadata' is defined but never used

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,5 @@
 import { getBlogPosts } from "@/data/blog-posts";
 import BlogClient from "./BlogClient";
-import { BlogPostMetadata } from "@/types/blog";
 
 export default async function BlogPage() {
   const posts = await getBlogPosts();
